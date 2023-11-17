8e118e6bdfc0bcc2d709eaac1d9a9cfee753d5a6
sewonjun <thornapple74@gmail.com>
2023-11-18 03:57:59 +0900

    chore: cors 해결을 위한 model 경로 수정

--- util/emotionPredictionModel.js
+++ util/emotionPredictionModel.js
@@ -1,9 +1,18 @@
 import * as tf from "@tensorflow/tfjs";
 
 async function emotionPredictionModel() {
-  const model = await tf.loadLayersModel(
-    "https://app.happythings.today/model/emotion-model.json"
+  const layerModel = await fetch(
+    `https://app.happythings.today/model/emotion-model.json`,
+    {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }
   );
+  const model = await tf.loadLayersModel(layerModel);
 
   return model;
 }
