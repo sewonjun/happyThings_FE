151694621b4efac431e5bcc285d718c4b5e321c2
sewonjun <thornapple74@gmail.com>
2023-11-18 04:13:06 +0900

    chore: model 경로 파일경로로 변경

--- util/emotionPredictionModel.js
+++ util/emotionPredictionModel.js
@@ -1,7 +1,7 @@
 import * as tf from "@tensorflow/tfjs";
 
 async function emotionPredictionModel() {
-  const model = await tf.loadLayersModel("../model/emotion-model.json");
+  const model = await tf.loadLayersModel("emotion-model.json");
 
   return model;
 }
