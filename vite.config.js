8265782c754414c2509e356eee245dcc4a6976b1
sewonjun <thornapple74@gmail.com>
2023-11-18 02:10:15 +0900

    chore: vite.config 변경

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,19 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  build: { chunkSizeWarningLimit: 1600 },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
