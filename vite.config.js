1de5e3e7fb6fb3289afd0f95edbc8d400fe28263
sewonjun <thornapple74@gmail.com>
2023-11-18 03:47:25 +0900

    chore: 빌드시 큰 용량을 차지하는 파일들을 위한 config 설정

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,11 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
+          if (id.includes("@tensorflow")) {
+            return "tensorflow";
+          }
+          if (id.includes("plotly.js-dist-min")) {
+            return "plotly";
           }
         },
       },
