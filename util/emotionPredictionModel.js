19ad089cea2e5a18b0c51a8318300c8389355b9a
sewonjun <thornapple74@gmail.com>
2023-11-18 03:37:18 +0900

    chore: model 경로 변경

--- util/emotionPredictionModel.js
+++ util/emotionPredictionModel.js
@@ -2,7 +2,7 @@ import * as tf from "@tensorflow/tfjs";
 
 async function emotionPredictionModel() {
   const model = await tf.loadLayersModel(
-    "http://app.happythings.today/model/emotion-model.json"
+    "https://app.happythings.today/model/emotion-model.json"
   );
 
   return model;
