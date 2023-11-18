9173debc3e82efbfdc529d54454191f2be4f1a88
sewonjun <thornapple74@gmail.com>
2023-11-18 23:25:34 +0900

    chore: model 경로 오타 수정

--- util/emotionPredictionModel.js
+++ util/emotionPredictionModel.js
@@ -2,7 +2,7 @@ import * as tf from "@tensorflow/tfjs";
 
 async function emotionPredictionModel() {
   const model = await tf.loadLayersModel(
-    "https://app.happythings.today/model/emotion-model.json"
+    "http://app.happythings.today/model/emotion-model.json"
   );
 
   return model;
