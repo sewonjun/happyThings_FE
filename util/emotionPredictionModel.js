c4df7be3371fb7fddc50f1ea128aca71f79bd6a1
sewonjun <thornapple74@gmail.com>
2023-11-18 20:43:06 +0900

    chore: model 경로 배포 주소로 바꾸기 -> 로컬호스트는 성공

--- util/emotionPredictionModel.js
+++ util/emotionPredictionModel.js
@@ -1,7 +1,9 @@
 import * as tf from "@tensorflow/tfjs";
 
 async function emotionPredictionModel() {
-  const model = await tf.loadLayersModel("../model/emotion-model.json");
+  const model = await tf.loadLayersModel(
+    "https://app./happythings.today/model/emotion-model.json"
+  );
 
   return model;
 }
