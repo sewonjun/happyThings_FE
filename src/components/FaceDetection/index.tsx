ccd955adcd5e4f99d50e694706f9ff07896e1b73
sewonjun <thornapple74@gmail.com>
2024-01-11 21:19:31 +0900

    chore: FaceDetection ts 마이그레이션

--- src/components/FaceDetection/index.tsx
+++ src/components/FaceDetection/index.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from "react";
-import vision from "@mediapipe/tasks-vision";
+import { FaceLandmarker, DrawingUtils } from "@mediapipe/tasks-vision";
 import { v4 as uuidv4 } from "uuid";
-const { FaceLandmarker, DrawingUtils } = vision;
 
 import initMediaPipe from "../../../mediaPipe/initMediaPipe";
 import predictHappiness from "../../../util/predictHappiness";
@@ -10,22 +9,38 @@ import CapturedImage from "../CapturedImage";
 import LoadingBtn from "../LoadingBtn";
 import emotionPredictionModel from "../../../util/emotionPredictionModel";
 
+type Emotion = "happy" | "unhappy" | "neutral" | null;
+
+interface FaceBlendShape {
+  index: number;
+  score: number;
+  categoryName: string;
+  displayName: string;
+}
+
+interface ImageRef {
+  capturedPicture: string;
+  faceBlendShape: FaceBlendShape[];
+}
+
 const FaceDetection = () => {
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
-  const captureRef = useRef(null);
-  const lastTime = useRef(0);
-  const imgRef = useRef([]);
-  const [faceLandmarker, setFaceLandmarker] = useState(null);
-  const [isMobile, setIsMobile] = useState(false);
-  const [webcamRunning, setWebcamRunning] = useState(false);
-  const [videoDetect, setVideoDetect] = useState(false);
-  const [animationId, setAnimationId] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [model, setModel] = useState(null);
-  const [emotion, setEmotion] = useState(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const captureRef = useRef<HTMLCanvasElement>(null);
+  const lastTime = useRef<number>(0);
+  const imgRef = useRef<ImageRef[]>([]);
+  const [faceLandmarker, setFaceLandmarker] = useState<FaceLandmarker | null>(
+    null
+  );
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const [webcamRunning, setWebcamRunning] = useState<boolean>(false);
+  const [videoDetect, setVideoDetect] = useState<boolean>(false);
+  const [animationId, setAnimationId] = useState<number | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [model, setModel] = useState<unknown>(null);
+  const [emotion, setEmotion] = useState<Emotion>(null);
   let imgRefNumber = 0;
-  let runningMode = "VIDEO";
+  const runningMode = "VIDEO";
 
   useEffect(() => {
     async function createFaceLandmarker() {
@@ -76,10 +91,10 @@ const FaceDetection = () => {
     setErrorMessage("");
   }
 
-  function handleFaceMask() {
+  async function handleFaceMask() {
     if (videoDetect && animationId) {
       window.cancelAnimationFrame(animationId);
-      videoRef.current.removeEventListener("loadeddata", predictWebcam);
+      videoRef.current!.removeEventListener("loadeddata", predictWebcam);
 
       setWebcamRunning(false);
       setVideoDetect(false);
@@ -91,7 +106,7 @@ const FaceDetection = () => {
     if (faceLandmarker && !videoDetect) {
       setVideoDetect(true);
 
-      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      if (navigator.mediaDevices) {
         enableCam();
       } else {
         alert("Camera is not supported on your device.");
@@ -110,14 +125,17 @@ const FaceDetection = () => {
       video: true,
     };
 
-    const openMediaDevices = async constraint => {
+    const openMediaDevices = async (constraint: MediaStreamConstraints) => {
       return await navigator.mediaDevices.getUserMedia(constraint);
     };
 
     try {
       const stream = await openMediaDevices(constraints);
-      videoRef.current.srcObject = stream;
-      videoRef.current.addEventListener("loadeddata", predictWebcam);
+
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+        videoRef.current.addEventListener("loadeddata", predictWebcam);
+      }
 
       if (runningMode === "VIDEO") {
         await faceLandmarker.setOptions({ runningMode: runningMode });
@@ -129,32 +147,38 @@ const FaceDetection = () => {
 
   async function predictWebcam() {
     const canvas = canvasRef.current;
-
-    if (canvas === null || webcamRunning === false) return;
-
     const video = videoRef.current;
-    const videoRect = videoRef.current.getBoundingClientRect();
 
-    let startTimeMs = performance.now();
+    if (
+      canvas === null ||
+      webcamRunning === false ||
+      video === null ||
+      faceLandmarker === null
+    )
+      return;
+
+    const videoRect = video.getBoundingClientRect();
+    const startTimeMs = performance.now();
     const results = await faceLandmarker.detectForVideo(video, startTimeMs);
 
     if (!results.faceLandmarks.length) {
       setErrorMessage("Face Detection Failed");
     }
 
-    canvas?.setAttribute("width", videoRect.width);
-    canvas?.setAttribute("height", videoRect.height);
-    canvas.style.left = videoRect.x;
-    canvas.style.top = videoRect.y;
-    canvas.style.width = videoRect.width;
-    canvas.style.height = videoRect.height;
-    const canvasCtx = canvasRef.current.getContext("2d");
-    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-    const drawingUtils = new DrawingUtils(canvasCtx);
+    canvas.setAttribute("width", videoRect.width.toString());
+    canvas.setAttribute("height", videoRect.height.toString());
+    // canvas.style.left = videoRect.x + "px";
+    // canvas.style.top = videoRect.y + "px";
+    canvas.style.width = videoRect.width + "px";
+    canvas.style.height = videoRect.height + "px";
+    const canvasCtx = canvas.getContext("2d");
 
-    if (results.faceLandmarks.length) {
+    if (canvasCtx) {
+      canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+      const drawingUtils = new DrawingUtils(canvasCtx!);
       drawFaceMask(results, drawingUtils, FaceLandmarker);
     }
+
     const currentTime = performance.now();
     const delay = 500;
 
@@ -162,43 +186,45 @@ const FaceDetection = () => {
       lastTime.current = currentTime;
 
       const capture = captureRef.current;
-      capture?.setAttribute("width", videoRect.width);
-      capture?.setAttribute("height", videoRect.height);
-      capture.style.left = videoRect.x;
-      capture.style.top = videoRect.y;
-      capture.style.width = videoRect.width;
-      capture.style.height = videoRect.height;
-      const captureCtx = captureRef.current.getContext("2d");
-      captureCtx.clearRect(0, 0, canvas.width, canvas.height);
-      captureCtx.drawImage(
-        videoRef.current,
-        0,
-        0,
-        videoRef.current.width,
-        videoRef.current.height
-      );
+      const video = videoRef.current;
 
-      capture.toBlob(async blob => {
-        const faceBlendShape = results.faceBlendshapes[0]?.categories;
-        const emotionResult = await predictHappiness(faceBlendShape, model);
-        setEmotion(emotionResult);
+      if (capture && video) {
+        captureRef.current.setAttribute("width", videoRect.width.toString());
+        captureRef.current.setAttribute("height", videoRect.height.toString());
+        captureRef.current.style.left = videoRect.x + "px";
+        captureRef.current.style.top = videoRect.y + "px";
+        captureRef.current.style.width = videoRect.width + "px";
+        captureRef.current.style.height = videoRect.height + "px";
+        const captureCtx = captureRef.current.getContext("2d");
 
-        if (emotionResult === "happy") {
-          const capturedPicture = URL.createObjectURL(blob);
-
-          imgRef.current[imgRefNumber] = {
-            capturedPicture,
-            faceBlendShape,
-          };
-
-          imgRefNumber++;
+        if (captureCtx) {
+          captureCtx.clearRect(0, 0, canvas.width, canvas.height);
+          captureCtx.drawImage(video, 0, 0, videoRect.width, videoRect.height);
         }
 
-        if (webcamRunning) {
-          const animationFrameId = window.requestAnimationFrame(predictWebcam);
-          setAnimationId(animationFrameId);
-        }
-      }, "image/png");
+        capture.toBlob(async blob => {
+          const faceBlendShape = results.faceBlendshapes[0]?.categories;
+          const emotionResult = await predictHappiness(faceBlendShape, model);
+          setEmotion(emotionResult);
+
+          if (emotionResult === "happy" && blob !== null) {
+            const capturedPicture = URL.createObjectURL(blob);
+
+            imgRef.current[imgRefNumber] = {
+              capturedPicture,
+              faceBlendShape,
+            };
+
+            imgRefNumber++;
+          }
+
+          if (webcamRunning) {
+            const animationFrameId =
+              window.requestAnimationFrame(predictWebcam);
+            setAnimationId(animationFrameId);
+          }
+        }, "image/png");
+      }
     } else {
       if (webcamRunning) {
         const animationFrameId = window.requestAnimationFrame(predictWebcam);
@@ -227,8 +253,8 @@ const FaceDetection = () => {
       )}
       {webcamRunning ? (
         <>
-          <div className="flex flex-col h-screen justify-center items-center">
-            <div className="flex flex-row bg-stone-200 border-4 border-stone-900 ring-offset-0 p-2 m-1 w-auto rounded-3xl justify-around">
+          <div className="flex flex-col h-screen items-center">
+            <div className="flex flex-row h-auto w-auto bg-stone-200 border-2 border-stone-900 ring-offset-0 p-2 m-1  rounded-3xl justify-around">
               <div
                 className={`text-4xl p-3 m-2 ${
                   emotion === "unhappy" ? "bg-red-600 " : "bg-stone-300"
@@ -256,27 +282,20 @@ const FaceDetection = () => {
             <div
               className={`${
                 isMobile
-                  ? "flex flex-col justify-center items-center w-full h-4/5 border-2 bg-stone-800"
+                  ? "flex flex-col justify-center items-center w-auto h-auto border-2 bg-stone-800"
                   : "flex flex-col justify-center items-center w-6/12 h-4/5 border-2 max-w-md bg-stone-800"
               }`}
             >
-              <div className="grid grid-cols">
-                <div className="relative block w-full h-[360px] border-y border-violet-950">
+              <div className="grid grid-cols w-full h-full">
+                <div className="relative block min-w-72 h-fit">
                   <video
                     ref={videoRef}
-                    width="410"
-                    height="360"
                     autoPlay
                     playsInline
-                    className="w-full h-[360px] bg-white border-8 border-stone-800"
+                    className="absolute w-fit h-fit"
                   ></video>
-                </div>
-                <div className="absolute block w-fit h-[360px] ">
-                  <canvas
-                    ref={canvasRef}
-                    height="360"
-                    className="block w-full h-[360px]"
-                  />
+                  <canvas ref={canvasRef} className="absolute w-fit h-fit" />
+                  <canvas ref={captureRef} className="hidden" />
                 </div>
                 <div className="block text-center items-center justify-center my-5 py-5">
                   <button
@@ -289,9 +308,6 @@ const FaceDetection = () => {
                 </div>
               </div>
             </div>
-            <div className="hidden">
-              <canvas ref={captureRef} />
-            </div>
           </div>
         </>
       ) : (
@@ -301,7 +317,7 @@ const FaceDetection = () => {
         />
       )}
       <div className="flex justify-center align-middle text-center text-2xl py-5">
-        {imgRef.length ? "Select one picture to make a polaroid" : ""}
+        {imgRef.current.length ? "Select one picture to make a polaroid" : ""}
       </div>
       <div className="flex flex-col justify-center align-middle text-center">
         {imgRef.current
