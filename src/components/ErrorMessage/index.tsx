4887e62dbe913a3e75eb5299d8fac7a7dcdbbdf1
sewonjun <thornapple74@gmail.com>
2024-02-15 21:54:17 +0900

    feat: 에러 메세지 관심사 분리

--- src/components/ErrorMessage/index.tsx
+++ src/components/ErrorMessage/index.tsx
@@ -3,19 +3,21 @@ interface ErrorMessageProp {
   handleRestart: () => void;
 }
 
-const ErrorMessage = ({ message, handleRestart }: ErrorMessageProp) => (
-  <div className="flex flex-col justify-center items-center mt-10 h-auto w-6/12 m-auto bg-red-500">
-    <h1 className="text-base text-gray-50 decoration-solid mb-1">
-      Error: {message}
-    </h1>
-    <button
-      onClick={handleRestart}
-      type="button"
-      className="text-gray-50 bg-black h-auto p-1 m-1"
-    >
-      Restart
-    </button>
-  </div>
-);
+function ErrorMessage({ message, handleRestart }: ErrorMessageProp) {
+  return (
+    <div className="flex flex-col justify-center items-center mt-10 h-auto w-6/12 m-auto bg-red-500">
+      <h1 className="text-base text-gray-50 decoration-solid mb-1">
+        Error: {message}
+      </h1>
+      <button
+        onClick={handleRestart}
+        type="button"
+        className="text-gray-50 bg-black h-auto p-1 m-1"
+      >
+        Restart
+      </button>
+    </div>
+  );
+}
 
 export default ErrorMessage;
